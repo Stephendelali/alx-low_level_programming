01197e42a3612865280e920cac5520e2c8e3ac48
Amankwa Delali Stephen <amankwastephen06@gmail.com>
2023-07-18 09:26:50 -0700

    update for nessted loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,23 +1,17 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98,
- * followed by a new line
- * @n: print from this number
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ *
+ * Return: sum of a and b
  */
-void print_to_98(int n)
+int add(int a, int b)
 {
-    if (n <= 98)
-    {
-        for (; n < 98; n++)
-            printf("%d, ", n);
-    }
-    else
-    {
-        for (; n > 98; n--)
-            printf("%d, ", n);
-    }
+	int c;
 
-    printf("%d\n", n);
+	c = a + b;
+
+	return (c);
 }
