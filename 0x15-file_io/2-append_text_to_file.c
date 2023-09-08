6b22413f8263fbf66442f0c3fc41a0911987ae33
Amankwa Delali Stephen <amankwastephen06@gmail.com>
2023-09-08 11:30:10 -0700

    file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,49 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * append_text_to_file - Appends text to the end of a file.
- * @filename: The name of the file.
- * @text_content: The string to add to the file.
+ * @name: Name file.
+ * @text_content: NULL terminated string to add at the end of the file.
  *
  * Return: 1 on success, -1 on failure.
  */
 
-int append_text_to_file(const char *filename, char *text_content);
+int append_text_to_file(const char *name, char *text_content)
+{
+    FILE *file;
+
+    if (name == NULL)
+        return (-1);
+
+    file = fopen(name, "a");
+
+    if (file == NULL)
+        return (-1);
+
+    if (text_content != NULL)
+    {
+        if (fputs(text_content, file) == EOF)
+        {
+            fclose(file);
+            return (-1);
+        }
+    }
+
+    fclose(file);
+    return (1);
+}
+
+/**
+ * main - Entry point.
+ * @argc: arg number
+ * @argv:  argument strings.
+ *
+ * Return: 0 on success, 1 on error.
+ */
 
 int main(int argc, char **argv)
 {
-	int result;
+    int result;
 
-	if (argc != 3)
-	{
-		fprintf(stderr, "Usage: %s filename text\n", argv[0]);
-		exit(1);
-	}
-	result = append_text_to_file(argv[1], argv[2]);
-	printf("-> %i\n", result);
-	return (0);
-}
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s filename text\n", argv[0]);
+        exit(1);
+    }
 
-int append_text_to_file(const char *filename, char *text_content)
-{
-	if (filename == NULL)
-		return (-1);
+    result = append_text_to_file(argv[1], argv[2]);
+    printf("-> %d\n", result);
 
-	FILE *file = fopen(filename, "a");
-	if (file == NULL)
-		return (-1);
-
-	if (text_content != NULL)
-	{
-		if (fputs(text_content, file) == EOF)
-		{
-			fclose(file);
-			return (-1);
-		}
-	}
-
-	fclose(file);
-	return (1);
+    return (0);
 }
