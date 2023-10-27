a4763919d52834476532ee2cff08909b247d3557
Stephen <amankwastephen06@gmail.com>
2023-10-26 17:42:24 -0700

    git manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - changes binary num to int
+ * @c: string for a the binary num
+ *
+ * Return: changed num
+ */
+unsigned int binary_to_uint(const char *c)
+{
+	int i;
+	unsigned int dec_val = 0;
+
+	if (!c)
+		return (0);
+
+	for (i = 0; c[i]; i++)
+	{
+		if (c[i] < '0' || c[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (c[i] - '0');
+	}
+
+	return (dec_val);
+}
